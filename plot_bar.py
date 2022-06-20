83924c750a27505fb13358c7c856b3358dd50aa3
Yuanyuan <412890161@qq.com>
2022-06-20 14:06:11 -0700

    Modify plots for PD

--- plot_bar.py
+++ plot_bar.py
@@ -8,9 +8,9 @@ topology_small_map = {'geant': 'geant', 'dtelekom': 'dtelekom', 'abilene': 'abil
 
 
 algorithm = ['PrimalDual', 'Greedy1', 'Greedy2', 'Random1', 'Random2', 'Heuristic']
-Dirs = {1: ["OUTPUT6/", "Greedy1/CacheRoute/", "Greedy1/RouteCache/", "Random1/CacheRoute/", "Random1/RouteCache/", "Heuristic1/"],
-        2: ["OUTPUT8/", "Greedy2/CacheRoute/", "Greedy2/RouteCache/", "Random2/CacheRoute/", "Random2/RouteCache/", "Heuristic2/"],
-        3: ["OUTPUT9/", "Greedy3/CacheRoute/", "Greedy3/RouteCache/", "Random3/CacheRoute/", "Random3/RouteCache/", "Heuristic3/"]}
+Dirs = {1: ["OUTPUT10/", "Greedy1/CacheRoute/", "Greedy1/RouteCache/", "Random1/CacheRoute/", "Random1/RouteCache/", "Heuristic1/"],
+        2: ["OUTPUT11/", "Greedy2/CacheRoute/", "Greedy2/RouteCache/", "Random2/CacheRoute/", "Random2/RouteCache/", "Heuristic2/"],
+        3: ["OUTPUT12/", "Greedy3/CacheRoute/", "Greedy3/RouteCache/", "Random3/CacheRoute/", "Random3/RouteCache/", "Heuristic3/"]}
 
 colors = ['r', 'sandybrown', 'gold', 'darkseagreen', 'c', 'dodgerblue', 'm']
 hatches = ['/', '\\\\', '|', '+', '--', '', '////',  'x', 'o', '.', '\\']
@@ -20,6 +20,14 @@ def readresult(fname):
     with open(fname, 'rb') as f:
         result = pickle.load(f)
     return result
+
+
+def saveviolation(x, type, bandwidth_coefficient):
+    fname = 'Figure/top%d/topologies%f.txt' % (type, bandwidth_coefficient)
+    with open(fname, 'w') as f:
+        for k, v in x.items():
+            f.write(k + ': ' + str(v))
+            f.write('\n')
 
 
 def barplot(x, type, bandwidth_coefficient):
@@ -70,6 +78,12 @@ if __name__ == '__main__':
         for top in topology_small_map.values():
             obj[alg][top] = 0
 
+    violation = {}
+    for top in topology_map.values():
+        obj[top] = 0
+    for top in topology_small_map.values():
+        obj[top] = 0
+
     fname1 = "_%ditems_%dnodes_10querynodes_%ddemands_%dcapcity_%fbandwidth_%dstepsize" % (
         args.catalog_size, args.graph_size, args.demand_size, args.max_capacity, args.bandwidth_coefficient, args.stepsize)
 
@@ -88,8 +102,37 @@ if __name__ == '__main__':
 
         fname = Dir[0] + top + fname1
         result = readresult(fname)
-        compare = result[-1][-1]
-        obj[algorithm[0]][topology_map[top]] = result[-1][-1] / compare
+
+        '''calculate violation'''
+        SumFlows = []
+        NumNonzeroFlows = []
+        iterations, Xs, Rs, overflows, Duals, lagrangians, objs = zip(*result)
+        for overflow in overflows:
+            ActiveFlow = []
+            Flow = []
+            for e in overflow:
+                if overflow[e] > 0:  # violated flow
+                    ActiveFlow.append(overflow[e])
+                if overflow[e] > -1:  # non zero flow
+                    Flow.append(overflow[e])
+            if ActiveFlow:
+                SumFlows.append(sum(ActiveFlow))
+            else:
+                SumFlows.append(0)
+            if Flow:
+                NumNonzeroFlows.append(len(Flow))
+            else:
+                NumNonzeroFlows.append(0)
+        vios = np.array(SumFlows) / np.array(NumNonzeroFlows)
+        vio_min = min(vios)
+        violation[top] = vio_min
+
+        '''obj'''
+        compare = 0
+        for i in range(len(objs)):
+            if vios[i] == vio_min:
+                compare = max(compare, objs[i])
+        obj[algorithm[0]][topology_map[top]] = compare / compare
 
         for j in range(1, len(algorithm)-1):
             fname = Dir[j] + top + fname2
@@ -104,8 +147,37 @@ if __name__ == '__main__':
 
         fname = Dir[0] + top + fname3
         result = readresult(fname)
-        compare = result[-1][-1]
-        obj[algorithm[0]][topology_small_map[top]] = result[-1][-1] / compare
+
+        '''calculate violation'''
+        SumFlows = []
+        NumNonzeroFlows = []
+        iterations, Xs, Rs, overflows, Duals, lagrangians, objs = zip(*result)
+        for overflow in overflows:
+            ActiveFlow = []
+            Flow = []
+            for e in overflow:
+                if overflow[e] > 0:  # violated flow
+                    ActiveFlow.append(overflow[e])
+                if overflow[e] > -1:  # non zero flow
+                    Flow.append(overflow[e])
+            if ActiveFlow:
+                SumFlows.append(sum(ActiveFlow))
+            else:
+                SumFlows.append(0)
+            if Flow:
+                NumNonzeroFlows.append(len(Flow))
+            else:
+                NumNonzeroFlows.append(0)
+        vios = np.array(SumFlows) / np.array(NumNonzeroFlows)
+        vio_min = min(vios)
+        violation[top] = vio_min
+
+        '''obj'''
+        compare = 0
+        for i in range(len(objs)):
+            if vios[i] == vio_min:
+                compare = max(compare, objs[i])
+        obj[algorithm[0]][topology_small_map[top]] = compare / compare
 
         for j in range(1, len(algorithm)-1):
             fname = Dir[j] + top + fname4
@@ -116,5 +188,6 @@ if __name__ == '__main__':
         result = readresult(fname)
         obj[algorithm[-1]][topology_small_map[top]] = result[-1][-1] / compare
 
+    saveviolation(violation, args.bandwidth_type, args.bandwidth_coefficient)
     barplot(obj, args.bandwidth_type, args.bandwidth_coefficient)
 
--- plot_sens.py
+++ plot_sens.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import logging, argparse
 import pickle
+import numpy as np
 
 bandwidth_coefficients = [1, 1.5, 2, 2.5, 3]
 algorithm = ['PrimalDual', 'Greedy1', 'Greedy2', 'Random1', 'Random2', 'Heuristic']
-Dirs = {1: ["OUTPUT6/", "Greedy1/CacheRoute/", "Greedy1/RouteCache/", "Random1/CacheRoute/", "Random1/RouteCache/", "Heuristic1/"],
-        2: ["OUTPUT8/", "Greedy2/CacheRoute/", "Greedy2/RouteCache/", "Random2/CacheRoute/", "Random2/RouteCache/", "Heuristic2/"],
-        3: ["OUTPUT9/", "Greedy3/CacheRoute/", "Greedy3/RouteCache/", "Random3/CacheRoute/", "Random3/RouteCache/", "Heuristic3/"]}
+Dirs = {1: ["OUTPUT10/", "Greedy1/CacheRoute/", "Greedy1/RouteCache/", "Random1/CacheRoute/", "Random1/RouteCache/", "Heuristic1/"],
+        2: ["OUTPUT11/", "Greedy2/CacheRoute/", "Greedy2/RouteCache/", "Random2/CacheRoute/", "Random2/RouteCache/", "Heuristic2/"],
+        3: ["OUTPUT12/", "Greedy3/CacheRoute/", "Greedy3/RouteCache/", "Random3/CacheRoute/", "Random3/RouteCache/", "Heuristic3/"]}
 
 colors = ['r', 'sandybrown', 'gold', 'darkseagreen', 'c', 'dodgerblue', 'm']
 line_styles = ['s-', '*-', 'd--', '^-', 'v-', '.:']
@@ -80,7 +81,35 @@ if __name__ == '__main__':
 
         fname = Dir[0] + fname1
         result = readresult(fname)
-        result = result[-1][-1]
+
+        '''calculate violation'''
+        SumFlows = []
+        NumNonzeroFlows = []
+        iterations, Xs, Rs, overflows, Duals, lagrangians, objs = zip(*result)
+        for overflow in overflows:
+            ActiveFlow = []
+            Flow = []
+            for e in overflow:
+                if overflow[e] > 0:  # violated flow
+                    ActiveFlow.append(overflow[e])
+                if overflow[e] > -1:  # non zero flow
+                    Flow.append(overflow[e])
+            if ActiveFlow:
+                SumFlows.append(sum(ActiveFlow))
+            else:
+                SumFlows.append(0)
+            if Flow:
+                NumNonzeroFlows.append(len(Flow))
+            else:
+                NumNonzeroFlows.append(0)
+        vios = np.array(SumFlows) / np.array(NumNonzeroFlows)
+        vio_min = min(vios)
+
+        '''obj'''
+        result = 0
+        for i in range(len(objs)):
+            if vios[i] == vio_min:
+                result = max(result, objs[i])
         obj[algorithm[0]].append(result)
 
         for i in range(1, len(algorithm)-1):
